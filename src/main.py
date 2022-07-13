13cb29a5b40262173b45acd9fa7dd3dae7af0914
Catherine Master <cmaster@umd.edu>
2022-07-13 00:14:51 -0400

    Generated JSON file and updated API key storage

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 # Created: Feb 09 2022
 # Spotify Playlist Exporter
 
-from datetime import date
-import getpass
 import json
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
 
-CLIENT_ID = ""
-CLIENT_KEY = ""
 URI = "http://127.0.0.1:9090"
 SCOPE = "playlist-read-private"
+CLIENT_ID = ""
+CLIENT_KEY = ""
 USERNAME = ""
 
-# Fields:
-# Playlist Name
-# No. of tracks
-# Creation date
-# All tracks in the playlist
+
+def configure():
+    load_dotenv()
+
+    global CLIENT_ID, CLIENT_KEY, USERNAME
+    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_KEY = os.getenv("CLIENT_KEY")
+    USERNAME = os.getenv("USERNAME")
+
+
+def getPlaylistName(sp, playlistId):
+    res = sp.playlist(playlistId)
+    return res["name"]
 
 
 def getArtists(data):
@@ -39,6 +47,7 @@ def getTrack(data):
 
 def getPlaylistTracks(sp, userId, playlistId):
     res = sp.user_playlist_tracks(userId, playlistId)
+
     trackData = res["items"]
     playlistTracks = []
 
@@ -63,16 +72,26 @@ def getPlaylistTracks(sp, userId, playlistId):
 
 
 def main():
+    configure()
+
+    filename = str(input("Enter filename: "))
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                          client_secret=CLIENT_KEY, redirect_uri=URI, scope=SCOPE))
 
     results = sp.current_user_playlists()
-    playlistIds = []
-    for p in results["items"]:
-        playlistIds.append(p["id"])
+    data = {}
 
-    playlistTracks = getPlaylistTracks(sp, USERNAME, "53AX7LCWWDdEAjrE5Fh8cR")
-    # print(playlistTracks)
+    for p in results["items"]:
+        id = p["id"]
+        name = getPlaylistName(sp, id)
+        tracklist = getPlaylistTracks(sp, USERNAME, id)
+
+        playlistInfo = {"id": id, "data": tracklist}
+
+        data[name] = playlistInfo
+
+    with open("%s.json" % filename, "w") as fp:
+        json.dump(data, fp)
 
 
 if __name__ == "__main__":
